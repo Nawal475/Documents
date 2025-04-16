617671146f49e5a6eb2d84b024213a083a4fcabf
nawal <nawal@101032300088.com>
2025-04-16 06:43:44 +0000

    First

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import unittest
+from app import hello
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(),"Hello, World!")
+if __name__ == "__main__":
+    unittest.main()
+import unittest
+from app import hello
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(),"Hello, World!")
+if __name__ == "__main__":
+    unittest.main()
